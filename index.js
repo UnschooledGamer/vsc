a693b4dfade2f5eebbed3d77ef083053b3a45fe4
UnschooledGamer <76094069+UnschooledGamer@users.noreply.github.com>
2021-06-24 12:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json')
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.login("")
+client.on('message', message => { 
+  if (message.content === `${config.prefix}ping`) {
+    message.channel.send("pong");
+  } 
+});
+client.login("ODQ2MzgxNzM4MTQyMDcyODMz.YKusaw.8NkWmZxAg-pdpWa8Zgs5_LW-wAQ")
