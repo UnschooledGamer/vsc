337911c02f50988ddff88d38e9d8ad9b5ad67853
UnschooledGamer <unschooledgamer@gmail.com>
2021-06-24 06:30:37 +0000

    enter

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(c
+
+client.on('ready', () => {
+console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(config.token)
